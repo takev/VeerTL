f622572ed68a84c12aef1f9f5722bafadc00be12
Tjienta Vara <tjientavara@users.noreply.github.com>
2018-12-17 16:00:03 +0100

    Add setup.py and main script

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+
+setup(
+    name="VeerTL",
+    version="1.0",
+    description="Template Language for code generation",
+    author="Tjienta Vara",
+    url="https://github.com/tjienta/VeerTL/",
+    packages=["VeerTL"],
+    scripts=["veer"]
+)
